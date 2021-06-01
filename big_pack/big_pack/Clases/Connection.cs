04bffb63c26709ac75e7584219b000ec89bc8b63
unknown <lorp.lop@yandex.ru>
2021-06-01 16:59:23 +0300

    sdds

---
+++ big_pack/big_pack/Clases/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace big_pack.Clases
+{
+    class Connection
+    {
+        //строка для подключения к базе данных
+        public static string String { get; } = "Data Source = WSR; Initial Catalog = user7; user = user7; password = passuser7";
+    }
+}
